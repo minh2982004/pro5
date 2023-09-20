4f33195a8bd2a1a96fdb4ec73a1d2e8466c80c27
minh2982004 <144196654+minh2982004@users.noreply.github.com>
2023-09-20 11:05:41 +0700

    Add files via upload

---
+++ J1.L.P0022/src/j1/l/p0022/Main.java
@@ -0,0 +1,15 @@
+package j1.l.p0022;
+
+import controller.Manager;
+import view.MenuModel;
+import view.MenuView;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MenuModel model = new MenuModel();
+        MenuView view = new MenuView(model);
+        Manager manager = new Manager(model, view);
+        manager.startMenu();
+    }
+}
